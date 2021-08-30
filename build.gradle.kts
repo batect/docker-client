0ef6fb97498badeea71e24187e62e18e45ec6d80
Charles Korn <me@charleskorn.com>
2021-08-30 15:28:08 +1000

    Add license header to Kotlin files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,19 @@
+/*
+    Copyright 2017-2021 Charles Korn.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 plugins {
     id("com.diffplug.spotless")
 }
@@ -10,6 +26,22 @@ tasks.named<Wrapper>("wrapper") {
     distributionType = Wrapper.DistributionType.ALL
 }
 
+val licenseText = """
+   Copyright 2017-2021 Charles Korn.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+""".trimIndent()
+
 allprojects {
     afterEvaluate {
         if (extensions.findByName("spotless") == null) {
@@ -17,16 +49,22 @@ allprojects {
         }
 
         val isKotlinProject = plugins.hasPlugin("org.jetbrains.kotlin.multiplatform")
+        val kotlinLicenseHeader = "/*\n${licenseText.lines().joinToString("\n") { "    $it".trimEnd() }}\n*/\n\n"
 
         spotless {
+            encoding("UTF-8")
+
             kotlinGradle {
                 ktlint()
+                licenseHeader(kotlinLicenseHeader, "plugins|rootProject|import")
             }
 
             if (isKotlinProject) {
                 kotlin {
                     target(fileTree("src").include("**/*.kt"))
+
                     ktlint()
+                    licenseHeader(kotlinLicenseHeader)
                 }
             }
         }
