8f2a2514a1dddac12bde44882233c9d8537e8887
Charles Korn <me@charleskorn.com>
2022-02-21 16:19:51 +1100

    Add another fix for Docker's non-deterministic behaviour.

--- client/src/commonTest/kotlin/batect/dockerclient/DockerClientBuildKitImageBuildSpec.kt
+++ client/src/commonTest/kotlin/batect/dockerclient/DockerClientBuildKitImageBuildSpec.kt
@@ -444,7 +444,14 @@ class DockerClientBuildKitImageBuildSpec : ShouldSpec({
         progressUpdatesReceived shouldContain StepStarting(1, "[internal] load build definition from Dockerfile")
         progressUpdatesReceived shouldContain StepStarting(2, "[internal] load .dockerignore")
         progressUpdatesReceived shouldContain StepStarting(3, "[internal] load metadata for docker.io/library/alpine:3.14.2")
-        progressUpdatesReceived shouldContainAnyOf setOf(StepStarting(4, "[other 1/2] FROM docker.io/library/alpine:3.14.2"), StepStarting(4, "[other 1/2] FROM docker.io/library/alpine:3.14.2@sha256:e1c082e3d3c45cccac829840a25941e679c25d438cc8412c2fa221cf1a824e6a"))
+
+        progressUpdatesReceived shouldContainAnyOf setOf(
+            StepStarting(4, "[other 1/2] FROM docker.io/library/alpine:3.14.2"),
+            StepStarting(4, "[other 1/2] FROM docker.io/library/alpine:3.14.2@sha256:e1c082e3d3c45cccac829840a25941e679c25d438cc8412c2fa221cf1a824e6a"),
+            StepStarting(4, "[stage-1 1/2] FROM docker.io/library/alpine:3.14.2"),
+            StepStarting(4, "[stage-1 1/2] FROM docker.io/library/alpine:3.14.2@sha256:e1c082e3d3c45cccac829840a25941e679c25d438cc8412c2fa221cf1a824e6a")
+        )
+
         progressUpdatesReceived shouldContain StepStarting(5, "[other 2/2] RUN touch /file-from-other")
         progressUpdatesReceived shouldContain StepStarting(6, "[stage-1 2/2] COPY --from=other /file-from-other /received/file-from-other")
         progressUpdatesReceived shouldContain StepStarting(7, "exporting to image")
