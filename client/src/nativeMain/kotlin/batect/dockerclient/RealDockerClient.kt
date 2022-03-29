4265ca95e4706b9ddb7dbae044e8df447370e5ab
Charles Korn <me@charleskorn.com>
2022-03-29 18:37:51 +1100

    Remove debugging output.

--- client/src/nativeMain/kotlin/batect/dockerclient/RealDockerClient.kt
+++ client/src/nativeMain/kotlin/batect/dockerclient/RealDockerClient.kt
@@ -336,7 +336,6 @@ internal actual class RealDockerClient actual constructor(configuration: DockerC
 
     override fun startContainer(container: ContainerReference) {
         StartContainer(clientHandle, container.id.cstr).ifFailed { error ->
-            println("Starting container failed: ${error.pointed.Message!!.toKString()}")
             throw ContainerStartFailedException(error.pointed)
         }
     }
