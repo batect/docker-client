b10a0e2e9ea62eb3920e7478299b75e21c627c1e
Charles Korn <me@charleskorn.com>
2023-02-25 15:25:07 +1100

    Fix further issue running tests against latest Docker version.

--- client/src/commonTest/kotlin/batect/dockerclient/DockerClientBuildKitImageBuildSpec.kt
+++ client/src/commonTest/kotlin/batect/dockerclient/DockerClientBuildKitImageBuildSpec.kt
@@ -573,6 +573,7 @@ class DockerClientBuildKitImageBuildSpec : ShouldSpec({
             }
 
             progressUpdatesReceived shouldContainAnyOf setOf(
+                BuildFailed("process \"/bin/sh -c echo \\\"This command has failed!\\\" && exit 1\" did not complete successfully: exit code: 1"),
                 BuildFailed("executor failed running [/bin/sh -c echo \"This command has failed!\" && exit 1]: exit code: 1"),
                 BuildFailed("failed to solve with frontend dockerfile.v0: failed to build LLB: executor failed running [/bin/sh -c echo \"This command has failed!\" && exit 1]: runc did not terminate sucessfully"),
             )
